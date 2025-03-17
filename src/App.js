ac4d5d554ab2c9a8ee35b2c55c183673bf3b5581
Pranavan Subendiran <129349835+Pranavan-S@users.noreply.github.com>
2025-03-17 23:34:23 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let's cook.
+        </p>
+        <p>
+          Hello-World!
         </p>
         <a
           className="App-link"
